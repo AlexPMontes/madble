f029b68c7342b4a4c43a2ba65c0e8e4feea2a6e9
Alex Pérez <100499978@alumnos.uc3m.es>
2023-11-02 23:01:53 +0100

    Feat: First version

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"crypto/tls"
+	"flag"
+	"fmt"
+	"os"
+
+	"layeh.com/gumble/gumble"
+	_ "layeh.com/gumble/opus"
+)
+
+func main() {
+	// Command line flags
+	server := flag.String("server", "localhost:64738", "the server to connect to")
+	username := flag.String("username", "", "the username of the client")
+	password := flag.String("password", "", "the password of the server")
+	insecure := flag.Bool("insecure", false, "skip server certificate verification")
+	certificate := flag.String("certificate", "", "PEM encoded certificate and private key")
+
+	flag.Parse()
+
+	// Initialize
+	b := Barnard{
+		Config:  gumble.NewConfig(),
+		Address: *server,
+	}
+
+	b.Config.Username = *username
+	b.Config.Password = *password
+
+	if *insecure {
+		b.TLSConfig.InsecureSkipVerify = true
+	}
+	if *certificate != "" {
+		cert, err := tls.LoadX509KeyPair(*certificate, *certificate)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%s\n", err)
+			os.Exit(1)
+		}
+		b.TLSConfig.Certificates = append(b.TLSConfig.Certificates, cert)
+	}
+
+	b.start()
+}
